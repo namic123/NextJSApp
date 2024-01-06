819ca3d6c3f6af61a39df1fe1a9dc9791d5cc5e1
jay sung <namic123@naver.com>
2024-01-06 20:00:01 +0900

    next js, 필요없는 코드 삭제

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
-import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,6 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
+    {/* children이라는 property는 page.js라는 파일의 값을 가져옴 */}
       <body>{children}</body>
     </html>
   )
