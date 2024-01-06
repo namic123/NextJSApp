3d7ebc985c2d377b6c8c6db83806a2208118f65a
jay sung <namic123@naver.com>
2024-01-06 20:27:56 +0900

    dynamic 라우팅 개념.

---
+++ src/app/read/[id]/page.js
@@ -0,0 +1,9 @@
+export default function Read(){
+  return (
+    /* URL이 /read/1 ~ /read/1000000 와 같은 경우, read폴더 안에 id폴더 안에 page.js가 들어간다. */
+    /* 이때, id는 props.params.id로 불러옴 */
+    <>
+    <h2>Read</h2>
+    parameters : {props.params.id}
+  </>)
+}
