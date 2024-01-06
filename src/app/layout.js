53059e8366c5a8a35d76bce503473b8d3d8130fe
jay sung <namic123@naver.com>
2024-01-06 20:10:24 +0900

    기본적인 next js app 뼈대 구성

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,32 @@ export default function RootLayout({ children }) {
   return (
     <html>
     {/* children이라는 property는 page.js라는 파일의 값을 가져옴 */}
-      <body>{children}</body>
+    {/* 공통된 내용은 layout 파일에서 작성한다.*/}
+      <body>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+      </ol>
+      {children}
+      <ul>
+        <li>
+          <a href="/create">Create</a>
+        </li>
+        <li>
+          <a href="/update/1">Update</a>
+        </li>
+        <li>
+          <input type="button" value="delete"/>
+        </li>
+      </ul>
+      </body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-    <h2>Hello, Nextjs!</h2>
+    <h2>Welcome</h2>
+      Hello, WEB!
     </>
   )
 }
