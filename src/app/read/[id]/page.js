c198092948627aebfc1073db76bf2b7c9e70c3a9
jay sung <namic123@naver.com>
2024-01-07 01:25:41 +0900

    Read는 사용자와 상호작용하지 않으므로, 서버 컴포넌트에 적합할 수 있다.

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,9 +1,11 @@
-export default function Read(props){
+export default async function Read(props){
+  const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const topic = await resp.json();
   return (
     /* URL이 /read/1 ~ /read/1000000 와 같은 경우, read폴더 안에 id폴더 안에 page.js가 들어간다. */
     /* 이때, id는 props.params.id로 불러옴 */
     <>
-    <h2>Read</h2>
-    parameters : {props.params.id}
+    <h2>{topic.title}</h2>
+      {topic.body}
   </>)
 }
