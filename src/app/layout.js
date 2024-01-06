d5cc814b4678c0defcae3c928f6c0ae9f14463c4
jay sung <namic123@naver.com>
2024-01-06 23:16:33 +0900

    useState 오류. useState는 오직 클라이언트 컴포넌트에서만 실행

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import Link from "next/link";
+import {useEffect, useState} from "react";
 
 
 export const metadata = {
@@ -8,6 +9,13 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+  const [topics, setTopics] = useState()
+  useEffect(() => {
+    fetch('http://localhost:9999/topics').then(resp => resp.json())
+      .then(result =>{
+        setTopics(result);
+      })
+  }, []);
   return (
     <html>
     {/* children이라는 property는 page.js라는 파일의 값을 가져옴 */}
